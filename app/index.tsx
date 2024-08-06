7ef3c1548fc0be7571f9df21d884bdbb7d351239
mahirminhajk <kmahirminhaj@gmail.com>
2024-08-06 22:41:22 +0530

    init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
+
+const App = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-3xl font-pblack'>Sora!</Text>
+      <StatusBar style="auto" />
+      <Link className='text-red-600 text-3xl' href="/profile">Go To Profile</Link>
+    </View>
+  )
+}
+
+export default App
---
+++ app/profile.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const Profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Profile</Text>
+    </View>
+  )
+}
+
+export default Profile

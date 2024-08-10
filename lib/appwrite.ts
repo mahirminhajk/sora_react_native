5f82f0a512c7b422a18eb290a27ff2449e3dd87e
mahirminhajk <kmahirminhaj@gmail.com>
2024-08-10 11:01:05 +0530

    update: appwrite config file added

---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpoint: process.env.END_POINT,
+    platform: process.env.PLATFORM,
+    projectId: process.env.PROJECT_ID,
+    databaseId: process.env.DATABASE_ID,
+    userCollectionId: process.env.USER_COLLECTION_ID,
+    videoCollectionId : process.env.VIDEO_COLLECTION_ID,
+    storageId: process.env.STORAGE_ID,
+};

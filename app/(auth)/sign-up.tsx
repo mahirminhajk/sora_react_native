23f5bf9f6caa36f4833118876557693e48406586
mahirminhajk <kmahirminhaj@gmail.com>
2024-08-10 20:22:21 +0530

    update: basic auth

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,52 +1,97 @@
-import {  View, Text, ScrollView, Image } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import {images} from '@/constants'
-import FormField from '@/components/FormField'
-import { useState } from 'react'
-import CustomButton from '@/components/CustomButton'
-import { Link } from 'expo-router'
-import { createUser } from '@/lib/appwrite'
+import { images } from "@/constants";
+import FormField from "@/components/FormField";
+import { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import { Link, router } from "expo-router";
+import { createUser } from "@/lib/appwrite";
 
 const SignUp = () => {
-
   const [form, setForm] = useState({
-    username: '',
-    email: '',
-    password: ''
-  })
+    username: "",
+    email: "",
+    password: "",
+  });
   const [loading, setLoading] = useState(false);
 
   const submit = async () => {
-    await createUser(form.email, form.password, form.username)
+    if (!form.username || !form.email || !form.password)
+      return Alert.alert("Error", "Please fill all fields");
+    setLoading(true);
+    try {
+
+      const result = await createUser(form.email, form.password, form.username);
+
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    }finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <SafeAreaView className='bg-primary h-full'>
+    <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className='w-full justify-center min-h-[85vh] px-4 my-6'>
-          <Image source={images.logo} className='w-[115px] h-[35px]' resizeMode='contain' />
-          <Text className='text-2xl mt-10 font-psemibold text-white'>Sign Up To Sora</Text>
-          
-          <FormField title='Username' placeholder='Enter your username' value={form.username} handleChangeText={(e) => setForm({...form, username: e})}  otherStyle="mt-7" />
-          
-          <FormField title='Email' placeholder='Enter your email' value={form.email} handleChangeText={(e) => setForm({...form, email: e})}  otherStyle="mt-7" keyboardType="email-address" />
-
-          <FormField title='Password' placeholder='Enter your Password' value={form.password} handleChangeText={(e) => setForm({...form, password: e})}  otherStyle="mt-7"  />
-        
-        <CustomButton title='Sign Up' handlePress={submit} containStyles='mt-7' isLoading={loading} />
-        
-        <View className='justify-center pt-5 flex-row gap-2'> 
-          <Text className='text-lg text-gray-100 font-pregular'>
-            Have an account already ?
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            className="w-[115px] h-[35px]"
+            resizeMode="contain"
+          />
+          <Text className="text-2xl mt-10 font-psemibold text-white">
+            Sign Up To Sora
           </Text>
-          <Link href="/sign-in" className='text-lg font-psemibold text-secondary'>Sign in</Link>
-        </View>
 
+          <FormField
+            title="Username"
+            placeholder="Enter your username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyle="mt-7"
+          />
+
+          <FormField
+            title="Email"
+            placeholder="Enter your email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyle="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            placeholder="Enter your Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyle="mt-7"
+          />
+
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
+            containStyles="mt-7"
+            isLoading={loading}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already ?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign in
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,5 @@
-import { Account, Client, ID } from "react-native-appwrite";
+import SignIn from "@/app/(auth)/sign-in";
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
 
 export const appwriteConfig = {
   endpoint: process.env.END_POINT,
@@ -19,15 +20,58 @@ client
   .setPlatform(appwriteConfig.platform);
 
 const account = new Account(client);
+const avatar = new Avatars(client);
+const databases = new Databases(client);
 
-export const createUser = async (email, password, username) => {
-  // Register User
-  account.create(ID.unique(), "me@example.com", "password", "Jane Doe").then(
-    function (response) {
-      console.log(response);
-    },
-    function (error) {
-      console.log(error);
+export const createUser = async (
+  email: string,
+  password: string,
+  username: string
+) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) {
+      throw new Error("Account not created");
     }
-  );
+
+    const avatarUrl = await avatar.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+export const signIn = async (email: string, password: string) => {
+  try {
+    const session = await account.createEmailPasswordSession(
+      email,
+      password
+    );
+    return session;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
 };
